3ba90f504d38f703b0cf1ce460273a2cf949b194
jain1601aman <54712796+jain1601aman@users.noreply.github.com>
2021-03-17 15:22:37 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:5000/*', { transport : ['websocket', 'polling', 'flashsocket'] })
+const socket = io('http://localhost:5000/', { transport : ['websocket', 'polling', 'flashsocket'] })
 const messageform = document.getElementById('send-container');
 const messagecontainer = document.getElementById('message-container');
 const chatmsg = document.getElementById('message-input');
