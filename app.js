549469daa7411d264e733870369e129979cf4a31
jain1601aman <54712796+jain1601aman@users.noreply.github.com>
2021-03-19 17:28:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const express = require('express')
 const app = express()
 var hash = require('pbkdf2-password')()
 var session = require('express-session');
-// const functions = require("firebase-functions");
-var http = app.listen(process.env.PORT)
+var mongoose = require('mongoose')
+var http = app.listen(process.env.PORT || 5000)
 const io = require('socket.io')(http, {
   cors: {
     origin: "*",
   },
 })
+// db connection
+var url = "mongodb+srv://admin:admin@4321@secure-chat.xjmni.mongodb.net/secure-chat"
+mongoose.connect(url, {useNewUrlParser : true , useUnifiedTopology : true}).then(()=>{console.log('db connected')}).catch((e)=>{console.log(e.message)});
+var db = mongoose.connection
+// db models
+var rooms = require('./rooms');
+
+//check empty object
+function isEmptyObject(obj) 
+{
+    return !Object.keys(obj).length;
+}
 
 app.use('/static',express.static('./static/'));
 app.set('views','./views')
@@ -32,22 +44,19 @@ app.use(function(req, res, next){
   next();
 });
 
+function debug() { console.log('yaha aayaa')}
+// // dummy database
 
-// dummy database
 
-var userss = {
-  tj: { name: 'tj' }
-};
+// // when you create a user, generate a salt
+// // and hash the password ('foobar' is the pass here)
 
-// when you create a user, generate a salt
-// and hash the password ('foobar' is the pass here)
-
-hash({ password: 'foobar' }, function (err, pass, salt, hash) {
-  if (err) throw err;
-  // store the salt & hash in the "db"
-  userss.tj.salt = salt;
-  userss.tj.hash = hash;
-});
+// hash({ password: 'foobar' }, function (err, pass, salt, hash) {
+//   if (err) throw err;
+//   // store the salt & hash in the "db"
+//   userss.tj.salt = salt;
+//   userss.tj.hash = hash;
+// });
 
 var removeByAttr = function(arr, attr, value){
     var i = arr.length;
@@ -74,59 +83,95 @@ function restrict(req, res, next) {
 
 function authenticate(name, pass, fn) {
   // if (!module.main) console.log('authenticating %s:%s', name, pass);
-  var user = userss[name];
-  if (!user) {
-    console.log('yaha aayaa')
-    return fn(new Error('cannot find user'));}
-  hash({ password: pass, salt: user.salt }, function (err, pass, salt, hash) {
-    if (err) return fn(err);
-    if (hash === user.hash) return fn(null, user)
-    fn(new Error('invalid password'));
-  });
+  rooms.find({roomid:name},(err,userdata)=>{
+    
+    if(err) throw err
+    
+    if(isEmptyObject(userdata))
+    {
+      return fn(new Error('cannot find user'));
+    }
+    else
+    {
+      var user = userdata[0]
+      hash({ password: pass, salt: user.salt }, function (err, pass, salt, hash) {
+        if (err) return fn(err);
+        if (hash === user.hash)
+        {          
+          return fn(null, user)
+        }
+        fn(new Error('invalid password'));
+      });
+    }
+
+  })
+  
 }
 var userdata = []
 var usersss = {}
+
+const Schema = mongoose.Schema;
+const roomdbs = new Schema({
+    socketid : String,
+    name : String,
+    pubkey : String
+});
+
 io.on('connection', socket =>{
     // console.log('new user')
-    socket.on('new-user' , user =>{
-      userdata.push({
-        id : socket.id,
-        Name : user.name,
-        pubkey : user.pubkey
-      })
+    var roomdb
+    var dbroom
+    socket.on('new-user' , (room,user) =>{
+      dbroom = room
+      roomdb = mongoose.model(room, roomdbs);
+      var newuser = new roomdb()
+      socket.join(room)
+        newuser.socketid = socket.id
+        newuser.name = user.name,
+        newuser.pubkey = user.pubkey
+        newuser.save()
       usersss[socket.id] = user.name
       })
       socket.on('get-key' , user =>{
-        var i = 0;
-        var x = userdata[i]
-        if(x.Name==user)
-        {
-          i=1
-        }
-        x = userdata[i]
-        socket.emit('take-key',x.pubkey)
-        
+        roomdb.find((err,userdata)=>{
+          if(err) throw err
+          var i = 0;
+          var x = userdata[i]
+          if(x.name==user)
+          {
+            i=1
+          }
+          x = userdata[i]
+          socket.emit('take-key',x.pubkey)
+        })
       })
-socket.on('send-chat-message' , data =>{
-    socket.broadcast.emit('send' , {user:usersss[socket.id],data:data})
+socket.on('send-chat-message' , (room,data) =>{
+    socket.to(room).broadcast.emit('send' , {user:usersss[socket.id],data:data})
 })
 socket.on('disconnect',()=>{
+  roomdb.deleteOne({socketid:socket.id} ,(err)=>{
+    if(err) throw err
+  })
+  roomdb.find((err,userdb)=>{
+    if(err) throw err
+    if(userdb.length === 0)
+    {
+      roomdb.collection.drop()
+      rooms.deleteOne({roomid:dbroom} , (err)=>{
+        if(err) throw err
+      })
+    }
+  })
   delete usersss[socket.id]
   removeByAttr(userdata, 'id', socket.id);
 })
 
 })
-
 app.get('/' , function(req,res){
-    res.render('index');
+    res.render('index' ,{id:'s',createroommessge:' '});
 })
-app.post('/' , function(req,res){
-  console.log(req.body.room)
-  console.log(req.body.pswd)
-  res.redirect('/')
-})
-app.get('*/login', restrict, function(req, res){
-  res.render('room');
+app.get('*/room/:roomID', restrict, function(req, res){
+  res.render('room' , {rid:req.params.roomID});
 });
 
 app.post('*/login', function(req, res){
@@ -134,23 +179,55 @@ app.post('*/login', function(req, res){
     if (user) {
       req.session.regenerate(function(){
         req.session.user = user;
-        req.session.success = 'Authenticated as ' + user.name
+        req.session.success = 'Authenticated as ' + user.roomid
           + ' click to <a href="/logout">logout</a>. '
           + ' You may now access <a href="/restricted">/restricted</a>.';
-        res.redirect('login');
+          var urln = 'room/' + req.body.username
+        res.redirect(urln);
       });
     } else {
-      console.log('yaha aayaa')
+      
       req.session.error = 'Authentication failed, please check your '
         + ' username and password.'
         + ' (use "tj" and "foobar")';
-      res.render('index');
+        s = 'Invalid Room ID or Password'
+        res.render('index' , {id:'f',createroommessge:s})
     }
   });
 });
-app.get('*/logout', function(req, res){
+app.get('/logout', function(req, res){
   req.session.destroy(function(){
     res.redirect('/');
   });
 });
+
+app.post('*/newroom',  function(req,res){
+  var id = req.body.room
+  var pass = req.body.pswd
+  rooms.find({roomid:id} ,async (err,user)=>{
+    if(err) throw err
+    if(isEmptyObject(user))
+    {
+      
+      hash({ password: pass }, function (err, passwd, salt, hash) {
+        if (err) throw err;
+        // store the salt & hash in the "db"
+        var newroom = new rooms()
+        newroom.roomid = id
+        newroom.salt = salt;
+        newroom.hash = hash;
+        newroom.save()
+      });
+      
+      s = 'Room created, please login with correct Room ID and Password'
+      res.render('index' , {id:'s',createroommessge:s})
+      
+    }
+    else
+    {
+      s = 'Room already exist, please login or try another combination for Room ID'
+      res.render('index' , {id:'f',createroommessge:s})
+    }
+  })
+})
 http
